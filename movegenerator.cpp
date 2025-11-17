dd47466a961b756a52ac9e70980cd41cd600eabc
nelletuemani <160103106+nelletuemani@users.noreply.github.com>
2025-11-16 20:15:08 -0800

    Update movegenerator.cpp for pawns

--- movegenerator.cpp
+++ movegenerator.cpp
@@ -4,3 +4,13 @@ void MoveGenerator::generateKingMoves(Bitboard& bb, long allies, long empty, boo
     U64 king = isWhiteToMove ? bb.wking : bb.bking;
     //moves.push_back()
 }
+void MoveGenerator::generatePawnMoves(Bitboard& bb, U64 enemy, U64 empty, int enPassant, bool isWhiteToMove, vector<Move>& moves)
+{
+    U64 pawns = white ? bb.whitePawns : bb.blackPawns;
+    int direction      = white ? 8 : -8;    
+    int startRank      = white ? 1 : 6;     
+    int promoRank      = white ? 6 : 1;    
+    int epCaptureLeft  = white ? 7 : -9;    
+    int epCaptureRight = white ? 9 : -7; 
+}
+
