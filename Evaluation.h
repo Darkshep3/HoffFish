1f0fa28283ad2013a9badd4061b94f065b3b291f
Darkshep3 <allenca9999@gmail.com>
2025-11-23 20:55:21 -0800

    Hw #3 or is this 4

---
+++ Evaluation.h
@@ -0,0 +1,9 @@
+//Piece Square Table 
+//Find a good pst - > 
+//Would a piece be worth the same during a midgame or endgame 
+//Penalties - isolated
+// Connected Pawns for bonus 
+
+//double evaluate (Bitboard bb, boolean isWhiteTomove)
+
+    //return score;
