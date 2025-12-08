38c3e995c83fb02702566272a20ad967ced30daa
Arush Borole <arushborole@gmail.com>
2025-12-07 18:16:59 -0800

    fixed include errors, added magic_generator.h, implemented play function in main

---
+++ magic_generator.h
@@ -0,0 +1,10 @@
+#include <cstdint> 
+#include <cstdlib> 
+#include <iostream>
+#include "magic.h"
+
+U64 randomU64();
+U64 randomU64_fewbits();
+int count_1s(U64 b);
+U64 find_magic (int sq, int m, int bishop);
+int magic_generator();
